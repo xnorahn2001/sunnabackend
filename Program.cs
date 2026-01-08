46548330f33f1a0dd1ae97249f33b30b57bf4046
Norah Alnaif <norahalnaif@MacBook-Air-Norah.local>
2026-01-08 05:45:12 +0300

    Add auto-migration on startup

--- Program.cs
+++ Program.cs
@@ -87,4 +87,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Auto-Run Migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try 
+    {
+        var context = services.GetRequiredService<SonnaDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 app.Run();
